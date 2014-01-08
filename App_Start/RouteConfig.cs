4e4705dc9dd1793b3607cf1c356ab78020f19b2f
Bibby <bibbynet@gmail.com>
2014-01-08 13:45:45 +0800

    Init

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UIRouterExample
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ui-route1",
+                url: "{controller}/{action}/r/{p1}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    p1 = UrlParameter.Optional,
+                }
+            );
+            routes.MapRoute(
+                name: "ui-route2",
+                url: "{controller}/{action}/r/{p1}/{p2}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    p1 = UrlParameter.Optional,
+                    p2 = UrlParameter.Optional,
+                }
+            );
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UIRouterExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RenderPartial()
+        {
+            var p1 = Convert.ToString(RouteData.Values["p1"]);
+            var p2 = Convert.ToString(RouteData.Values["p2"]);
+
+            var name = string.Format("{0}{1}", p1, p2);
+
+            if (string.IsNullOrEmpty(name))
+                return Content(string.Empty);
+
+            return PartialView(name);
+
+            //var html = RenderPartialToString(name, null);
+            //@ViewBag.Data = html;
+            //return Content(html);
+        }
+    }
+}
