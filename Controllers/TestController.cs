988910b898e2bbfa03d7098f1adc493d76cedd72
Bibby <bibbynet@gmail.com>
2014-01-23 23:36:19 +0800

    add new test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UIRouterExample.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Main/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PageData1()
+        {
+            var param = Convert.ToString(RouteData.Values["pathInfo"]);
+
+            string data = string.Format("category1, param:{0} - {1}", param, DateTime.Now);
+            return Content(data);
+        }
+
+        public ActionResult PageData2()
+        {
+            var param = Convert.ToString(RouteData.Values["pathInfo"]);
+            
+            string data = string.Format("category2, param:{0} - {1}", param, DateTime.Now);
+            return Content(data);
+        }
+    }
+}
