4c14105f3889c0256837613d5c714a0b315617a8
Bibby <bibbynet@gmail.com>
2014-01-25 17:27:54 +0800

    add multiple views test

---
+++ Controllers/MultiViewsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UIRouterExample.Controllers
+{
+    public class MultiViewsController : Controller
+    {
+        //
+        // GET: /MultiViews/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
