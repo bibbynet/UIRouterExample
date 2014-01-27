727f0528faf202ecc2111551262f29e1b5a6100d
Bibby <bibbynet@gmail.com>
2014-01-27 09:09:15 +0800

    SPA case

---
+++ Controllers/SPAController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UIRouterExample.Controllers
+{
+    public class SPAController : Controller
+    {
+        //
+        // GET: /SPA/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _Header()
+        {
+            return PartialView();
+        }
+    }
+}
